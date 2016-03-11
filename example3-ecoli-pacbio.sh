4e112a88bc6afaeafe4c2c380a86d277f1161b8c
Ivan Sovic <ivan.sovic@gmail.com>
2016-03-11 10:31:35 +0100

    Added another example script.

---
+++ example3-ecoli-pacbio.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+make modules
+make tools
+make -j
+
+### E. Coli:
+folder=test-data/DATASETS_FOR_CONSENSUS/ecoli_pacbio
+awk '$1 ~/S/ {print ">"$2"\n"$3}' $folder/layout.gfa > $folder/layout.fasta
+contigs=$folder/layout.fasta
+reads=$folder/reads.fastq
+sam=$folder/alignments.sam
+reference=$folder/ecoli_K12_MG1655_U00096.3.fasta
+# sam=temp/alignments.sam
+dataset=ecoli-pacbio
+msa=mafft
+# msa=poav2
+consensus=temp/consensus-${dataset}-${msa}.fasta
+memtime=temp/consensus-${dataset}-${msa}.memtime
+tools/graphmap/bin/Linux-x64/graphmap -a anchor -z 0 -c 40 -C -B 0 -r ${contigs} -d ${reads} -o ${sam}
+mkdir -p temp
+/usr/bin/time --format "Command line: %C\nReal time: %e s\nCPU time: -1.0 s\nUser time: %U s\nSystem time: %S s\nMaximum RSS: %M kB\nExit status: %x" --quiet -o $memtime \
+	bin/consise -w 500 --msa ${msa} -b 200 -t 16 --minnewseq 0.80 --maxovl 0.01 --winpath temp/window.fasta ${contigs} ${sam} ${consensus}
+mkdir -p temp/dnadiff-${dataset}
+rm temp/dnadiff-${dataset}/consise-${dataset}-${msa}.report
+dnadiff -p temp/dnadiff-${dataset}/consise-${dataset}-${msa} ${reference} ${consensus}
+grep "AlignedBases" temp/dnadiff-${dataset}/consise-${dataset}-${msa}.report
+grep "AvgIdentity" temp/dnadiff-${dataset}/consise-${dataset}-${msa}.report
+cat $memtime
