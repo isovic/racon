fc5d7df8fa7eed6f31e8d27d17f5c41df4f35873
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2016-04-30 17:06:12 +0200

    Updated the example scripts.

--- example2-ecoli.sh
+++ example2-ecoli.sh
@@ -4,6 +4,8 @@ make modules
 make tools
 make -j
 
+threads=4
+
 ### E. Coli:
 # awk '$1 ~/S/ {print ">"$2"\n"$3}' test-data/DATASETS_FOR_CONSENSUS/ecoli_map006_ont/layout.gfa > test-data/DATASETS_FOR_CONSENSUS/ecoli_map006_ont/layout.fasta
 # contigs=test-data/DATASETS_FOR_CONSENSUS/ecoli_map006_ont/layout.fasta
@@ -17,10 +19,10 @@ msa=poa
 consensus=temp/consensus-${dataset}-${msa}.fasta
 memtime=temp/consensus-${dataset}-${msa}.memtime
 reference=test-data/DATASETS_FOR_CONSENSUS/ecoli_map006_ont/ecoli_K12_MG1655_U00096.3.fasta
-tools/graphmap/bin/Linux-x64/graphmap align -a anchor -z 0 -c 40 -B 0 -r ${contigs} -d ${reads} -o ${sam} --extcigar
+tools/graphmap/bin/Linux-x64/graphmap align -a anchor -z 0 -c 40 -B 0 -r ${contigs} -d ${reads} -o ${sam} --extcigar -t ${threads}
 mkdir -p temp
 /usr/bin/time --format "Command line: %C\nReal time: %e s\nCPU time: -1.0 s\nUser time: %U s\nSystem time: %S s\nMaximum RSS: %M kB\nExit status: %x" --quiet -o $memtime \
-	bin/consise --align 1 -M 1 -X -1 -G -1 -E -1 --bq 10 -w 500 --ovl-margin 0.0 --msa ${msa} -b 200 -t 4 --winpath temp/window.fasta ${contigs} ${sam} ${consensus}
+	bin/consise --align 1 -M 1 -X -1 -G -1 -E -1 --bq 10 -w 500 --ovl-margin 0.0 --msa ${msa} -b 200 -t ${threads} --winpath temp/window.fasta ${contigs} ${sam} ${consensus}
 mkdir -p temp/dnadiff-${dataset}
 rm temp/dnadiff-${dataset}/consise-${dataset}-${msa}.report
 dnadiff -p temp/dnadiff-${dataset}/consise-${dataset}-${msa} ${reference} ${consensus}
--- example5-gfa-ecoli.sh
+++ example5-gfa-ecoli.sh
@@ -14,3 +14,13 @@ threads=4
 memtime=temp/gfacons-ecoli.memtime
 /usr/bin/time --format "Command line: %C\nReal time: %e s\nCPU time: -1.0 s\nUser time: %U s\nSystem time: %S s\nMaximum RSS: %M kB\nExit status: %x" --quiet -o $memtime \
 	scripts/gfacons.py $gfa $reads $consensus $threads $reference
+mkdir -p temp/dnadiff-${dataset}
+rm temp/dnadiff-${dataset}/consise-gfacons-${dataset}-${msa}.report
+dnadiff -p temp/dnadiff-${dataset}/consise-gfacons-${dataset}-${msa} ${reference} ${consensus}
+grep "TotalBases" temp/dnadiff-${dataset}/consise-gfacons-${dataset}-${msa}.report
+grep "AlignedBases" temp/dnadiff-${dataset}/consise-gfacons-${dataset}-${msa}.report
+grep "AvgIdentity" temp/dnadiff-${dataset}/consise-gfacons-${dataset}-${msa}.report
+cat $memtime
+
+tools/edlib/src/aligner ${consensus} ${reference} -p -f NICE > ${consensus}.refalign.txt
+head -n 10 ${consensus}.refalign.txt | tail -n 1
