f38c63bb819adf20f94ea5eda8cf87f1d850449e
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2016-05-31 23:05:41 +0200

    Updated the output of evalcontigs.sh script.

--- scripts/evalcontigs.py
+++ scripts/evalcontigs.py
@@ -174,17 +174,16 @@ def eval_contigs(ref_path, contig_path, temp_folder, generate_kmer_spectrum=Fals
 	avg_accuracy_overall /= float(len(seqs_contigs));
 	avg_id_overall /= float(len(seqs_contigs));
 
-	log('Draft assembly: "%s"\n' % (contig_path), sys.stderr, silent=silent);
+	log('Draft assembly: "%s"' % (contig_path), sys.stderr, silent=silent);
 	log('Overall average ID for the draft assembly: %f%%' % (avg_id_overall), sys.stderr, silent=silent);
 	log('Overall average accuracy for the draft assembly: %f%%' % (100.0*avg_accuracy_overall), sys.stderr, silent=silent);
-	log('', sys.stderr);
+	log('', sys.stderr, silent=silent);
 
-	# sys.stdout.write('=============================================\n');
 	sys.stdout.write('================= Summary ===================\n');
-	sys.stdout.write('Draft assembly: "%s"\n' % (contig_path));
+	sys.stdout.write('Draft assembly: "%s"' % (contig_path));
 	sys.stdout.write('Overall average ID for the draft assembly: %f%%\n' % (avg_id_overall));
 	sys.stdout.write('Overall average accuracy for the draft assembly: %f%%\n' % (100.0*avg_accuracy_overall));
-	sys.stderr.write('\n');
+	sys.stdout.write('=============================================\n\n');
 
 
 
