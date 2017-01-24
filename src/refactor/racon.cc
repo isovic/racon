547d072e95457a6760c12aeb0332d67fb110a00e
Ivan Sovic <ivan.sovic@gmail.com>
2017-01-24 11:35:20 +0100

    Working on the refactor.

--- src/refactor/racon.cc
+++ src/refactor/racon.cc
@@ -23,6 +23,24 @@ Racon::Racon(const Parameters& param) {
 Racon::Racon(const SequenceFile& reads, const SequenceFile& targets) {
 }
 
+int Racon::PopulateJobsConsensus_(const SequenceFile &refs, int64_t window_len, std::deque<Job> &jobs) const {
+  if (window_len <= 0) {
+  	return 1;
+  }
+
+  for (int64_t i=0; i<((int64_t) refs.get_sequences().size()); i++) {
+  	auto s = refs.get_sequences()[i];
+  	int64_t s_len = (int64_t) s->get_sequence_length();
+    for (size_t j=0; j<s_len; j+=window_len) {
+      int64_t start = j;
+      int64_t end = std::min(start + window_len, s_len);
+   	  jobs.push_back(Job(i, start, end));
+    }
+  }
+
+  return 0;
+}
+
 void Racon::HashNames_(const SequenceFile &seqs, MapId &id) const {
   for (size_t i=0; i<seqs.get_sequences().size(); i++) {
     const auto& s = seqs.get_sequences()[i];
--- src/refactor/racon.h
+++ src/refactor/racon.h
@@ -12,10 +12,22 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <deque>
 #include "parameters.h"
 #include "sequences/sequence_file.h"
 
 namespace is {
+
+class Job {
+ public:
+  explicit Job(int64_t ref_id, int64_t start, int64_t end) : ref_id_(ref_id), start_(start), end_(end) { }
+  ~Job() { };
+
+ private:
+  int64_t ref_id_;
+  int64_t start_;
+  int64_t end_;
+};
 
 class Racon;
 
@@ -35,9 +47,23 @@ class Racon {
   Racon(const Parameters& param);
   Racon(const SequenceFile& reads, const SequenceFile& targets);
 
+  /** A helper function to fill a map in which the key is a sequence
+  	* name, and the value is the ordinal number of the sequence in
+  	* the SequenceFile.
+  */
   void HashNames_(const SequenceFile &seqs, MapId &id) const;
+
+  /** Populates the job queue for consensus-type parallelization.
+  	* This means that each window of the contig is presented as a
+  	* single job.
+  	* @refs Reference sequences from which to create jobs.
+  	* @jobs The deque to which to append the jobs.
+  */
+  int PopulateJobsConsensus_(const SequenceFile &refs, int64_t window_len, std::deque<Job> &jobs) const;
+
   MapId read_id_;
   MapId target_id_;
+  std::deque<Job> jobs_;
 };
 
 }
