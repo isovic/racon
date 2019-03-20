104d1baf82c85313aafa430551c56843034b0e6f
Joyjit Daw <jdaw@nvidia.com>
2019-03-20 18:29:03 -0400

    [consensus] adding comments to consensus generation kernel

--- nvidia/cudapoa/src/cudapoa_generate_consensus.cu
+++ nvidia/cudapoa/src/cudapoa_generate_consensus.cu
@@ -20,6 +20,10 @@ uint16_t branchCompletion(uint16_t max_score_id_pos,
                          int16_t* predecessors)
 {
     uint16_t node_id = graph[max_score_id_pos];
+
+    // Go through all the outgoing edges of the node, and for
+    // each of the end nodes of the edges clear the scores
+    // for all the _other_ nodes that had edges to that end node.
     uint16_t out_edges = outgoing_edge_count[node_id];
     for(uint16_t oe = 0; oe < out_edges; oe++)
     {
@@ -30,18 +34,23 @@ uint16_t branchCompletion(uint16_t max_score_id_pos,
             uint16_t id = incoming_edges[out_node_in_edges * CUDAPOA_MAX_NODE_EDGES + ie];
             if (id != node_id)
             {
-                scores[node_id] = -1;
+                scores[id] = -1;
             }
         }
     }
 
     int32_t max_score = 0;
     uint16_t max_score_id = 0;
+    // Run the same node weight traversal algorithm as always, to find the new
+    // node with maximum weight.
+    // We can start from the very next position in the graph rank because
+    // the graph is topologically sorted and hence guarantees that successor of the current max
+    // node will be processed again.
     for(uint16_t graph_pos = max_score_id_pos + 1; graph_pos < node_count; graph_pos++)
     {
         node_id = graph[graph_pos];
-        predecessors[node_id] = -1;
 
+        predecessors[node_id] = -1;
         int32_t score_node_id = -1;
 
         uint16_t in_edges = incoming_edge_count[node_id];
@@ -80,6 +89,9 @@ uint16_t branchCompletion(uint16_t max_score_id_pos,
     return max_score_id;
 }
 
+
+// Device function to generate consensus from a given graph.
+// The input graph needs to be topologically sorted.
 __device__
 void generateConsensus(uint8_t* nodes,
                          uint16_t node_count,
@@ -94,6 +106,7 @@ void generateConsensus(uint8_t* nodes,
                          int32_t* scores,
                          uint8_t* consensus)
 {
+    // Initialize scores and predecessors to default value.
     for(uint16_t i = 0; i < node_count; i++)
     {
         predecessors[i] = -1;
@@ -105,12 +118,16 @@ void generateConsensus(uint8_t* nodes,
 
     for(uint16_t graph_pos = 0; graph_pos < node_count; graph_pos++)
     {
-        //printf("%d\n", max_score_id);
         uint16_t node_id = graph[graph_pos];
-        //printf("node id %d\n", node_id);
         uint16_t in_edges = incoming_edge_count[node_id];
 
         int32_t score_node_id = scores[node_id];
+
+        // For each node, go through it's incoming edges.
+        // If the weight of any of the incoming edges is greater
+        // than the score of the current node, or if the weight is equal
+        // but the predecessors of the edge are heavier than the current node,
+        // then update the score of the node to be the incoming edge weight.
         for(uint16_t e = 0; e < in_edges; e++)
         {
             uint16_t edge_w = incoming_edge_w[node_id * CUDAPOA_MAX_NODE_EDGES + e];
@@ -124,11 +141,14 @@ void generateConsensus(uint8_t* nodes,
             }
         }
 
+        // Then update the score of the node to be the sum
+        // of the score of the predecessor and itself.
         if (predecessors[node_id] != -1)
         {
             score_node_id += scores[predecessors[node_id]];
         }
 
+        // Keep track of the highest weighted node.
         if (max_score < score_node_id)
         {
             max_score_id = node_id;
@@ -138,6 +158,8 @@ void generateConsensus(uint8_t* nodes,
         scores[node_id] = score_node_id;
     }
 
+    // If the node with maximum score isn't a leaf of the graph
+    // then run a special branch completion function.
     if (outgoing_edge_count[max_score_id] != 0)
     {
         while(outgoing_edge_count[max_score_id] != 0)
