3885d6fb516ab474d3249388fb08b16da54ede1c
Joyjit Daw <jdaw@nvidia.com>
2019-05-01 07:13:27 -0700

    [cudatest] run test from any folder

--- test/cuda_end2end/cuda_test.sh
+++ test/cuda_end2end/cuda_test.sh
@@ -3,10 +3,15 @@
 SCRIPT_DIRECTORY=`dirname $0`
 DATA="${HOME}/ont-racon-data/nvidia"
 RESULT_FILE="test-output.txt"
-GOLDEN_FILE="golden-output.txt"
+GOLDEN_FILE="$SCRIPT_DIRECTORY/golden-output.txt"
 
 if [ -f $RESULT_FILE ]; then
     rm $RESULT_FILE
+fi
+
+if [ ! -f $GOLDEN_FILE ]; then
+    echo "Could not find golden value file at $GOLDEN_FILE"
+    exit 1
 fi
 
 CMD="$SCRIPT_DIRECTORY/racon -c7 -m 8 -x -6 -g -8  -w 500 -t 24 -q -1 $DATA/iterated_racon/reads.fa.gz $DATA/iterated_racon/reads2contigs_1_1.paf.gz $DATA/canu.contigs.fasta"
