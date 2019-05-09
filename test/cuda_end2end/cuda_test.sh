f6beb0f786deac91a0365febe8464a2df3b3b444
Joyjit Daw <jdaw@nvidia.com>
2019-05-09 12:42:37 -0700

    [test] add optional batches argument

--- test/cuda_end2end/cuda_test.sh
+++ test/cuda_end2end/cuda_test.sh
@@ -5,6 +5,12 @@ DATA="${HOME}/ont-racon-data/nvidia"
 RESULT_FILE="test-output.txt"
 GOLDEN_FILE="$SCRIPT_DIRECTORY/golden-output.txt"
 
+if [ $# -eq 0 ]; then
+    BATCHES=7
+else
+    BATCHES=$1
+fi
+
 if [ -f $RESULT_FILE ]; then
     rm $RESULT_FILE
 fi
@@ -14,7 +20,7 @@ if [ ! -f $GOLDEN_FILE ]; then
     exit 1
 fi
 
-CMD="$SCRIPT_DIRECTORY/racon -c7 -m 8 -x -6 -g -8  -w 500 -t 24 -q -1 $DATA/iterated_racon/reads.fa.gz $DATA/iterated_racon/reads2contigs_1_1.paf.gz $DATA/canu.contigs.fasta"
+CMD="$SCRIPT_DIRECTORY/racon -c${BATCHES} -m 8 -x -6 -g -8  -w 500 -t 24 -q -1 $DATA/iterated_racon/reads.fa.gz $DATA/iterated_racon/reads2contigs_1_1.paf.gz $DATA/canu.contigs.fasta"
 echo "Running command:"
 echo $CMD
 $CMD > ${RESULT_FILE}
