42700b9428a820cd2b91dafa7ab977446c2a5a40
Ivan Sovic <ivan.sovic@gmail.com>
2020-06-25 19:03:02 +0200

    Implemented a generic window splitting function generate_window_breakpoints in util.*. It breaks the alignment into supplied windows.

--- src/util.cpp
+++ src/util.cpp
@@ -8,80 +8,158 @@
 
 namespace racon {
 
-std::vector<std::tuple<uint32_t, uint32_t, uint32_t>> find_breaking_points_from_cigar(
-            const std::string& cigar, std::vector<IntervalInt64> windows)
+// Tuple of: <start, end, window_id>
+// using WindowInterval = std::vector<std::tuple<uint32_t, uint32_t, uint32_t>>;
+
+// std::vector<std::tuple<uint32_t, uint32_t, uint32_t>> find_breaking_points_from_cigar(
+//             const std::string& cigar, int32_t q_start, int32_t t_start,
+//             std::vector<IntervalInt64> windows)
+// {
+// }
+
+/*
+ * \param windows Vector of tuples, where each tuple element is: <start, end, window_id>.
+*/
+std::vector<WindowInterval> generate_window_breakpoints(
+            const std::string& cigar, int64_t q_start, int64_t t_start,
+            std::vector<std::tuple<int64_t, int64_t, int64_t>> windows)
 {
-    std::sort(windows.begin(), windows.end(), [](const IntervalInt64& a, const IntervalInt64& b) {
-        return a.start < b.start;
+    std::vector<WindowInterval> ret;
+
+    if (windows.empty()) {
+        return ret;
+    }
+
+    std::sort(windows.begin(), windows.end(),
+                [](const std::tuple<int64_t, int64_t, int64_t>& a,
+                    const std::tuple<int64_t, int64_t, int64_t>& b) {
+        return std::get<0>(a) < std::get<0>(b);
     });
 
-    std::vector<std::tuple<uint32_t, uint32_t, uint32_t>> ret;
+    // Avoid using the operators.
+    const char* cigar_str = cigar.c_str();
+    int64_t num_windows = static_cast<int64_t>(windows.size());
 
-    // // find breaking points from cigar
-    // std::vector<int32_t> window_ends;
-    // uint32_t w_offset = 0;
-    // for (uint32_t i = 0; i < t_end_; i += window_length) {
-    //     if (i > t_begin_) {
-    //         window_ends.emplace_back(i - 1);
-    //     } else {
-    //         ++w_offset;
-    //     }
-    // }
-    // window_ends.emplace_back(t_end_ - 1);
+    bool found_first_match = false;
+    std::tuple<int64_t, int64_t, int64_t> first_match = {0, 0, 0}, last_match = {0, 0, 0};
 
-    // uint32_t w = 0;
-    // bool found_first_match = false;
-    // std::tuple<uint32_t, uint32_t, uint32_t> first_match = {0, 0, 0}, last_match = {0, 0, 0};
+    // The "-1" is because of the while loops below (increment is done at the top).
+    int64_t q_pos = q_start - 1;
+    int64_t t_pos = t_start - 1;
+    int64_t curr_w = 0;
 
-    // int32_t q_ptr = (strand_ ? (q_length_ - q_end_) : q_begin_) - 1;
-    // int32_t t_ptr = t_begin_ - 1;
+    while (curr_w < num_windows && std::get<1>(windows[curr_w]) < t_start) {
+        ++curr_w;
+    }
 
-    // for (uint32_t i = 0, j = 0; i < cigar_.size(); ++i) {
-    //     if (cigar_[i] == 'M' || cigar_[i] == '=' || cigar_[i] == 'X') {
-    //         uint32_t k = 0, num_bases = atoi(&cigar_[j]);
-    //         j = i + 1;
-    //         while (k < num_bases) {
-    //             ++q_ptr;
-    //             ++t_ptr;
+    for (uint32_t i = 0, j = 0; i < cigar.size(); ++i) {
+        if (cigar_str[i] == 'M' || cigar_str[i] == '=' || cigar_str[i] == 'X') {
+            uint32_t num_bases = atoi(&cigar_str[j]);
+            j = i + 1;
+            for (uint32_t k = 0; k < num_bases; ++k) {
+                ++q_pos;
+                ++t_pos;
 
-    //             if (!found_first_match) {
-    //                 found_first_match = true;
-    //                 first_match = std::make_tuple(t_ptr, q_ptr, w + w_offset);
-    //             }
-    //             last_match = std::make_tuple(t_ptr + 1, q_ptr + 1, w + w_offset);
-    //             if (t_ptr == window_ends[w]) {
-    //                 if (found_first_match) {
-    //                     breaking_points_.emplace_back(first_match);
-    //                     breaking_points_.emplace_back(last_match);
-    //                 }
-    //                 found_first_match = false;
-    //                 ++w;
-    //             }
+                while (curr_w < num_windows && std::get<1>(windows[curr_w]) < t_pos) {
+                    ++curr_w;
+                }
+                if (curr_w >= num_windows) {
+                    break;
+                }
 
-    //             ++k;
-    //         }
-    //     } else if (cigar_[i] == 'I') {
-    //         q_ptr += atoi(&cigar_[j]);
-    //         j = i + 1;
-    //     } else if (cigar_[i] == 'D' || cigar_[i] == 'N') {
-    //         uint32_t k = 0, num_bases = atoi(&cigar_[j]);
-    //         j = i + 1;
-    //         while (k < num_bases) {
-    //             ++t_ptr;
-    //             if (t_ptr == window_ends[w]) {
-    //                 if (found_first_match) {
-    //                     breaking_points_.emplace_back(first_match);
-    //                     breaking_points_.emplace_back(last_match);
-    //                 }
-    //                 found_first_match = false;
-    //                 ++w;
-    //             }
-    //             ++k;
-    //         }
-    //     } else if (cigar_[i] == 'S' || cigar_[i] == 'H' || cigar_[i] == 'P') {
-    //         j = i + 1;
-    //     }
-    // }
+                const auto& win_start = std::get<0>(windows[curr_w]);
+                const auto& win_end = std::get<1>(windows[curr_w]);
+                const auto& win_id = std::get<2>(windows[curr_w]);
+
+                if (t_pos < win_start) {
+                    continue;
+                }
+
+                if (!found_first_match) {
+                    found_first_match = true;
+                    first_match = std::make_tuple(t_pos, q_pos, win_id);
+                }
+                last_match = std::make_tuple(t_pos + 1, q_pos + 1, win_id);
+                if (t_pos == (win_end - 1)) {
+                    if (found_first_match) {
+                        WindowInterval wi(
+                                std::get<1>(first_match),
+                                std::get<1>(last_match),
+                                std::get<0>(first_match),
+                                std::get<0>(last_match),
+                                std::get<2>(last_match));
+                        ret.emplace_back(std::move(wi));
+                    }
+                    found_first_match = false;
+                    ++curr_w;
+                    if (curr_w >= num_windows) {
+                        break;
+                    }
+                }
+            }
+        } else if (cigar_str[i] == 'I') {
+            q_pos += atoi(&cigar_str[j]);
+            j = i + 1;
+        } else if (cigar_str[i] == 'D' || cigar_str[i] == 'N') {
+            uint32_t num_bases = atoi(&cigar_str[j]);
+            j = i + 1;
+            for (uint32_t k = 0; k < num_bases; ++k) {
+                ++t_pos;
+
+                while (curr_w < num_windows && std::get<1>(windows[curr_w]) < t_pos) {
+                    ++curr_w;
+                }
+                if (curr_w >= num_windows) {
+                    break;
+                }
+
+                const auto& win_start = std::get<0>(windows[curr_w]);
+                const auto& win_end = std::get<1>(windows[curr_w]);
+
+                if (t_pos < win_start) {
+                    continue;
+                }
+
+                if (t_pos == (win_end - 1)) {
+                    if (found_first_match) {
+                        WindowInterval wi(
+                                std::get<1>(first_match),
+                                std::get<1>(last_match),
+                                std::get<0>(first_match),
+                                std::get<0>(last_match),
+                                std::get<2>(last_match));
+                        ret.emplace_back(std::move(wi));
+                    }
+                    found_first_match = false;
+                    ++curr_w;
+                    if (curr_w >= num_windows) {
+                        break;
+                    }
+                }
+            }
+        } else if (cigar_str[i] == 'S' || cigar_str[i] == 'H' || cigar_str[i] == 'P') {
+            j = i + 1;
+        }
+    }
+
+    // Add the final piece.
+    if (curr_w < static_cast<int64_t>(windows.size())) {
+        const auto& win_start = std::get<0>(windows[curr_w]);
+        const auto& win_end = std::get<1>(windows[curr_w]);
+        const auto& final_t_start = std::get<0>(first_match);
+        const auto& final_t_end = std::get<0>(last_match);
+        if (found_first_match && final_t_start >= win_start && final_t_end <= win_end) {
+            WindowInterval wi(
+                    std::get<1>(first_match),
+                    std::get<1>(last_match),
+                    std::get<0>(first_match),
+                    std::get<0>(last_match),
+                    std::get<2>(last_match));
+            ret.emplace_back(std::move(wi));
+        }
+    }
+
+    return ret;
 }
 
 }
--- src/util.hpp
+++ src/util.hpp
@@ -8,6 +8,7 @@
 
 #include <algorithm>
 #include <cstdint>
+#include <ostream>
 #include <vector>
 #include <unordered_map>
 #include <tuple>
@@ -18,6 +19,38 @@ using IntervalVectorInt64 = IntervalTreeInt64::interval_vector;
 using IntervalInt64 = IntervalTreeInt64::interval;
 
 namespace racon {
+
+class WindowInterval {
+public:
+    WindowInterval() = default;
+
+    WindowInterval(int64_t _q_start, int64_t _q_end, int64_t _t_start, int64_t _t_end, int64_t _window_id)
+        : q_start(_q_start)
+        , q_end(_q_end)
+        , t_start(_t_start)
+        , t_end(_t_end)
+        , window_id(_window_id)
+    {}
+
+    bool operator==(const WindowInterval& rhs) const
+    {
+        return q_start == rhs.q_start && q_end == rhs.q_end && t_start == rhs.t_start
+                    && t_end == rhs.t_end && window_id == rhs.window_id;
+    }
+
+    std::ostream& operator<<(std::ostream& os) const {
+        os << "q_start = " << q_start << ", q_end = " << q_end
+            << ", t_start = " << t_start << "t_end = " << t_end
+            << "window_id = " << window_id;
+        return os;
+    }
+
+    int64_t q_start = 0;
+    int64_t q_end = 0;
+    int64_t t_start = 0;
+    int64_t t_end = 0;
+    int64_t window_id = -1;
+};
 
 template<typename T>
 bool transmuteId(const std::unordered_map<T, uint64_t>& t_to_id, const T& t,
@@ -31,7 +64,8 @@ bool transmuteId(const std::unordered_map<T, uint64_t>& t_to_id, const T& t,
     return true;
 }
 
-std::vector<std::tuple<uint32_t, uint32_t, uint32_t>> find_breaking_points_from_cigar(
-            const std::string& cigar, std::vector<IntervalInt64> windows);
+std::vector<WindowInterval> generate_window_breakpoints(
+            const std::string& cigar, int64_t q_start, int64_t t_start,
+            std::vector<std::tuple<int64_t, int64_t, int64_t>> windows);
 
 }
